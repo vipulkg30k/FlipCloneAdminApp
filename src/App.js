6c83a7e7966ce3bc55e3492267905060b63d1685
Vipul <vipulkg30@gmail.com>
2021-05-04 10:53:11 +0530

    Categories can be added directly

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/HOC/PrivateRoute";
 import { useDispatch, useSelector } from "react-redux";
 import Products from "./containers/Products";
 import Orders from "./containers/Orders";
+import Category from "./containers/Category";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ function App() {
     <div className="App">
       <Switch>
         <PrivateRoute path="/" exact component={Home} />
+        <PrivateRoute path="/category" component={Category} />
         <PrivateRoute path="/products" component={Products} />
         <PrivateRoute path="/orders" component={Orders} />
         
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import userReducer from './user.reducer';
 import authReducer from './auth.reducers';
+import productReducer from './product.reducer';
+import categoryReducer from './category.reducer';
+import orderReducer from './order.reducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    user: userReducer
+    user: userReducer,
+    category: categoryReducer,
+    product: productReducer,
+    order: orderReducer
 });
 
 export default rootReducer; 
