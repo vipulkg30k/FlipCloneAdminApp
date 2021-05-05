41bd9ba402829ae8438fcd1104e22bb0283881b9
Vipul <vipulkg30@gmail.com>
2021-05-05 10:59:17 +0530

    Category and Product Added

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../Header';
 import { Container, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import './style.css';
+
 /**
 * @author
 * @function Layout
@@ -17,7 +19,7 @@ const Layout = (props) => {
             <Row>
               <Col md={2} className="sidebar">
                 <ul>
-                  <li><NavLink to={'/'}>Home</NavLink></li>
+                  <li><NavLink exact to={'/'}>Home</NavLink></li>
                   <li><NavLink to={'/category'}>Category</NavLink></li>
                   <li><NavLink to={'/products'}>Products</NavLink></li>
                   <li><NavLink to={'/orders'}>Orders</NavLink></li>
